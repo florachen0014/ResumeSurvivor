af85bdf3d5258829d359e9b609307fb600d46115
Flora Chen <33159502+florachen0014@users.noreply.github.com>
2020-08-25 17:37:46 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -43,27 +43,6 @@ def file_download_link(filename):
 
      
 @app.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     form = UploadForm()
-#     if form.validate_on_submit():
-#         filename = documents.save(form.document.data)
-        
-#         os.path.dirname(os.path.abspath('app.py'))
-#         file_url = documents.url(filename)
-#         resume.process(filename)
-#         df = pd.read_csv('single-resume.csv')
-        
-#         columns = df.columns # for a dynamically created table
-
-#         table_d = df.to_json(orient='index')    
-#         html = HTML(df.to_html())
-#         output = cosine.process(filename)
-
-#         return render_template('index.html',form=form,filename=filename,file_url=file_url,html=html,value=filename,out=output)
-#         return html(file_download_link(filename))
-#     else:
-#         file_url = None
-#     return render_template('index.html', form=form, file_url=file_url)
 
 def upload_file():
     form = UploadForm()
